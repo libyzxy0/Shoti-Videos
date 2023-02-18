a62b64ec9377617742374ce0fe93b11b642055cf
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-02-18 09:40:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const axios = require("axios");
 const shoti = (callback) => {
 	let a = axios.get('https://api.libyzxy0.repl.co/api/shoti');
 	a.then((response) => {
-    let video = response.data.result.url;
-    request({
-    url: video,
-    encoding: null
-    },
-    (err, resp, buffer) => {
-    	callback(err, resp);
-    });
+        let video = response.data.result.url;
+        request({
+           url: video,
+           encoding: null
+        },
+        (err, resp, buffer) => {
+    	   callback(err, resp);
+        });
 	});
 }
 app.get('/', (req, res) => {
 	shoti((err, resp) => {
-		if (!err && resp.statusCode === 200) {
-			res.set("Content-Type", "video/mp4");
+	if (!err && resp.statusCode === 200) {
+	    res.set("Content-Type", "video/mp4");
             res.send(resp.body);
         }
     });
